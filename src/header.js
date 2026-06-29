0aaa1ed3236da57c3604cd32311a7fe0c0d41f1b
keshavsoft <nknnkumar@yahoo.com>
2026-06-29 09:51:37 +0530

    action to test

--- src/header.js
+++ src/header.js
@@ -1,8 +1,8 @@
 // src/header.js
-import initHeader from "../bin/header/v5/commands/header/template/v10/initHeader.js";
+import initHeader from "../bin/header/v5/commands/header/template/v11/initHeader.js";
 
 (async () => {
-    window.KSHeaderVersion = "v5.10";
+    window.KSHeaderVersion = "v11";
 
     window.KSHeader = initHeader;
 })();

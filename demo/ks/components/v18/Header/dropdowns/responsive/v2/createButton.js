fb27f6c60544f4ddb24a6118c39d67ae2a021e0d
keshavsoft <nknnkumar@yahoo.com>
2026-07-10 10:27:28 +0530

    v2

---
+++ demo/ks/components/v18/Header/dropdowns/responsive/v2/createButton.js
@@ -0,0 +1,7 @@
+export const createButton = (element) => {
+    const button = document.createElement("button");
+    const customClass = element.getAttribute("ks-class") || "";
+    button.className = `${customClass} flex justify-between items-center w-full px-4 py-2 rounded-md hover:bg-gray-600 active:bg-gray-500 active:scale-95 transition-all duration-150 md:flex-row md:justify-center md:items-center lg:bg-transparent lg:px-2 lg:py-1 cursor-pointer`.trim();
+    button.type = "button";
+    return button;
+};
--- demo/ks/components/v18/Header/index.js
+++ demo/ks/components/v18/Header/index.js
@@ -1,7 +1,9 @@
 import "./NavBar/index.js";
 import "./MenuItem/index.js";
 import "./dropDownAbsolute/index.js";
-import "./dropDownResponsive/index.js";
+// import "./dropDownResponsive/index.js";
 import "./dropDownInline/index.js";
+
+import "./dropdowns/responsive/v2/index.js";
 
 export default {};
